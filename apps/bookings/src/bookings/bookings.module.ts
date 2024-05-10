c495b6e4195ebc9c5db33c5a6c8c43d880a515ad
lawalgodwin <godwinl200@gmail.com>
2024-05-10 23:28:11 +0100

    Refactor codebase by setting up .env file for each microservice

--- apps/bookings/src/bookings/bookings.module.ts
+++ apps/bookings/src/bookings/bookings.module.ts
@@ -5,9 +5,27 @@ import { DatabaseModule } from '@app/common/database';
 import { BookingsRepository } from './bookings.repository';
 import { Booking } from './entities/booking.entity';
 import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([Booking]), LoggerModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([Booking]),
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_PORT: Joi.number().required(),
+        MYSQL_DATABASE: Joi.string().required(),
+        MYSQL_USERNAME: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_SYNC: Joi.boolean().required(),
+        HTTP_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [BookingsController],
   providers: [BookingsService, BookingsRepository],
 })
--- apps/bookings/src/main.ts
+++ apps/bookings/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { BookingsModule } from './bookings/bookings.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(BookingsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('HTTP_PORT'));
 }
 bootstrap();
